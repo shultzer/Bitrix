8dc601cf5883284fdb3542de00b674c081ff83bb
Sergei <skorohods@mail.ru>
2017-12-02 14:58:12 +0300

    module restore pwd~

--- local/modules/my.module/include.php
+++ local/modules/my.module/include.php
@@ -1,49 +1,49 @@
 <?
 
-  use Bitrix\Main\Security\Random;
+    use Bitrix\Main\Security\Random;
 
-  IncludeModuleLangFile(__FILE__);
+    IncludeModuleLangFile(__FILE__);
 
-  class GeneratePassword {
+    class GeneratePassword {
 
-    public static function OnBeforeUserSendPasswordHandler($arFields) {
-      //проверяем на наличие введенных данных в базе и определяем  ид пользователя по введенным данным
-      if ($arFields[ 'LOGIN' ] != '') {
-        $user        = CUser::GetList($by = "personal_country", $order = "desc", ['login' => $arFields[ 'LOGIN' ]]);
-        $currentuser = $user->Fetch();
-        $userid      = $currentuser[ 'ID' ];
-        $usermail    = $currentuser[ 'EMAIL' ];
-      }
-      if ($arFields[ 'EMAIL' ] != '') {
-        $user        = CUser::GetList($by = "personal_country", $order = "desc", ['email' => $arFields[ 'EMAIL' ]]);
-        $currentuser = $user->Fetch();
-        $userid      = $currentuser[ 'ID' ];
-        $usermail    = $arFields[ 'EMAIL' ];
-      }
-      //если пользователь найден - генерируем пароль и обновляем запись в базе
-      if ($currentuser != FALSE) {
-        $new_pwd                    = '/9' . Random::getString(8, TRUE);
-        $arfields                   = [
-          "PASSWORD"         => $new_pwd,
-          "CONFIRM_PASSWORD" => $new_pwd,
-          "EMAIL_TO"         => $usermail,
-        ];
-        $upuser                     = new CUser;
-        $arResult[ 'updateresult' ] = $upuser->Update($userid, $arfields);
-      }
-      //если пароль записался в базу - отсылаем пользователю сообщение на почту свои шаблоны
-      if ($arResult[ 'updateresult' ]) {
-        $arEventFields = [
-          "EMAIL_TO" => $usermail,
-          "NEWPWD"   => $new_pwd,
-        ];
-        $rsMess        = CEventMessage::GetList($by = "site_id", $order = "desc", ["TYPE_ID" => "GENERATE_PWD"]);
-        while ($arMess = $rsMess->GetNext()) {
-          CEvent::Send("USER_INFO", SITE_ID, $arEventFields, "N", $arMess[ "ID" ]);
+        public static function OnBeforeUserSendPasswordHandler ($arFields) {
+            //проверяем на наличие введенных данных в базе и определяем  ид пользователя по введенным данным
+            if ( $arFields[ 'LOGIN' ] != '' ) {
+                $user        = CUser::GetList($by = "personal_country", $order = "desc", [ 'login' => $arFields[ 'LOGIN' ] ]);
+                $currentuser = $user->Fetch();
+                $userid      = $currentuser[ 'ID' ];
+                $usermail    = $currentuser[ 'EMAIL' ];
+            }
+            if ( $arFields[ 'EMAIL' ] != '' ) {
+                $user        = CUser::GetList($by = "personal_country", $order = "desc", [ 'email' => $arFields[ 'EMAIL' ] ]);
+                $currentuser = $user->Fetch();
+                $userid      = $currentuser[ 'ID' ];
+                $usermail    = $arFields[ 'EMAIL' ];
+            }
+            //если пользователь найден - генерируем пароль и обновляем запись в базе
+            if ( $currentuser != FALSE ) {
+                $new_pwd                    = '/9' . Random::getString(8, TRUE);
+                $arfields                   = [
+                  "PASSWORD"         => $new_pwd,
+                  "CONFIRM_PASSWORD" => $new_pwd,
+                  "EMAIL_TO"         => $usermail,
+                ];
+                $upuser                     = new CUser;
+                $arResult[ 'updateresult' ] = $upuser->Update($userid, $arfields);
+            }
+            //если пароль записался в базу - отсылаем пользователю сообщение на почту свои шаблоны
+            if ( $arResult[ 'updateresult' ] ) {
+                $arEventFields = [
+                  "EMAIL_TO" => $usermail,
+                  "NEWPWD"   => $new_pwd,
+                ];
+                $rsMess        = CEventMessage::GetList($by = "site_id", $order = "desc", [ "TYPE_ID" => "GENERATE_PWD" ]);
+                while ( $arMess = $rsMess->GetNext() ) {
+                    CEvent::Send("USER_INFO", SITE_ID, $arEventFields, "N", $arMess[ "ID" ]);
+                }
+
+            }
+            //запрещаем отсылку штатного сообщения с контрольной строкой
+            return FALSE;
         }
-       
-      }
-      //запрещаем отсылку штатного сообщения с контрольной строкой
-      return FALSE;
     }
-  }
--- local/modules/my.module/install/index.php
+++ local/modules/my.module/install/index.php
@@ -1,132 +1,132 @@
 <?
 
-  use Bitrix\Main\Localization\Loc;
-  use Bitrix\Main\ModuleManager;
-  use \Bitrix\Main\IO\Directory;
+    use Bitrix\Main\Localization\Loc;
+    use Bitrix\Main\ModuleManager;
+    use \Bitrix\Main\IO\Directory;
 
-  Loc::loadMessages(__FILE__);
+    Loc::loadMessages(__FILE__);
 
-  Class my_module extends CModule {
+    Class my_module extends CModule {
 
-    var $MODULE_ID;
+        var $MODULE_ID;
 
-    var $MODULE_VERSION;
+        var $MODULE_VERSION;
 
-    var $MODULE_VERSION_DATE;
+        var $MODULE_VERSION_DATE;
 
-    var $MODULE_NAME;
+        var $MODULE_NAME;
 
-    var $MODULE_DESCRIPTION;
+        var $MODULE_DESCRIPTION;
 
-    var $MODULE_CSS;
+        var $MODULE_CSS;
 
-    var $MODULE_GROUP_RIGHTS = "Y";
+        var $MODULE_GROUP_RIGHTS = "Y";
 
-    function __construct() {
-      $arModuleVersion = [];
+        function __construct () {
+            $arModuleVersion = [];
 
-      $path = str_replace("\\", "/", __FILE__);
-      $path = substr($path, 0, strlen($path) - strlen("/index.php"));
-      include($path . "/version.php");
+            $path = str_replace("\\", "/", __FILE__);
+            $path = substr($path, 0, strlen($path) - strlen("/index.php"));
+            include( $path . "/version.php" );
 
 
-      $this->MODULE_ID = "my.module";
-      if (is_array($arModuleVersion) && array_key_exists("VERSION", $arModuleVersion)) {
-        $this->MODULE_VERSION      = $arModuleVersion[ "VERSION" ];
-        $this->MODULE_VERSION_DATE = $arModuleVersion[ "VERSION_DATE" ];
-      }
-      $this->MODULE_NAME        = 'Отправка сгенерированного пароля на email';
-      $this->MODULE_DESCRIPTION = "Модуль отправки сгенерированного пароля на email пользователя";
-      $this->PARTNER_NAME       = "Скороход С.";
-      $this->PARTNER_URI        = "Скороход С.";
+            $this->MODULE_ID = "my.module";
+            if ( is_array($arModuleVersion) && array_key_exists("VERSION", $arModuleVersion) ) {
+                $this->MODULE_VERSION      = $arModuleVersion[ "VERSION" ];
+                $this->MODULE_VERSION_DATE = $arModuleVersion[ "VERSION_DATE" ];
+            }
+            $this->MODULE_NAME        = 'Отправка сгенерированного пароля на email';
+            $this->MODULE_DESCRIPTION = "Модуль отправки сгенерированного пароля на email пользователя";
+            $this->PARTNER_NAME       = "Скороход С.";
+            $this->PARTNER_URI        = "Скороход С.";
+        }
+
+        function InstallEvents () {
+
+
+            $obEventType = new CEventType;
+            $obEventType->Add([
+              "EVENT_NAME"  => "GENERATE_PWD",
+              "NAME"        => "Сгенерирован новый пароль",
+              "LID"         => "ru",
+              "DESCRIPTION" => "#NEWPWD# - Сгенерированный пароль",
+            ]);
+            $arSites = [];
+            $sites   = CSite::GetList($b = "", $o = "", [ LANGUAGE_ID => "ru" ]);
+            while ( $site = $sites->Fetch() ) {
+                $arSites[] = $site[ "LID" ];
+            }
+            if ( count($arSites) > 0 ) {
+                $ar[ "ACTIVE" ]     = "Y";
+                $ar[ "EVENT_NAME" ] = "GENERATE_PWD";
+                $ar[ "EMAIL_FROM" ] = "#DEFAULT_EMAIL_FROM#";
+                $ar[ "LID" ]        = $arSites;
+                $ar[ "EMAIL_TO" ]   = "#EMAIL_TO#";
+                $ar[ "SUBJECT" ]    = "Сгенерирован новый пароль";
+                $ar[ "BODY_TYPE" ]  = "text";
+                $ar[ "MESSAGE" ]    = "Вам сгенерирован новый пароль для входа. #NEWPWD#";
+                $emess              = new CEventMessage;
+                $emess->Add($ar);
+            }
+            return TRUE;
+        }
+
+        function UnInstallEvents () {
+            $obEventType = new CEventType;
+            $obEventType->Delete("GENERATE_PWD");
+            $arFilter = [
+              "TYPE_ID" => "GENERATE_PWD",
+            ];
+            $rsMess   = CEventMessage::GetList($by = "site_id", $order = "desc", $arFilter);
+
+            while ( $arMess = $rsMess->GetNext() ) {
+                $emess = new CEventMessage;
+                $emess->Delete($arMess[ 'ID' ]);
+            }
+            return TRUE;
+        }
+
+        function InstallDB () {
+            RegisterModuleDependences('main', 'OnBeforeUserSendPassword',
+              'my.module', 'GeneratePassword', 'OnBeforeUserSendPasswordHandler');
+
+            return TRUE;
+        }
+
+        function UnInstallDB () {
+
+            UnRegisterModuleDependences('main', 'OnBeforeUserSendPassword',
+              'my.module', 'GeneratePassword', 'OnBeforeUserSendPasswordHandler');
+
+            return TRUE;
+
+        }
+
+        function DoInstall () {
+
+            $this->InstallDB();
+            $this->InstallEvents();
+            ModuleManager::registerModule($this->MODULE_ID);
+        }
+
+        function DoUninstall () {
+            ModuleManager::unregisterModule($this->MODULE_ID);
+            $this->UnInstallEvents();
+            $this->UninstallDB();
+        }
+
+
+        public function getpath ($notdocumentroot = FALSE) {
+            if ( $notdocumentroot ) {
+                return str_ireplace(\Bitrix\Main\Application::getDocumentRoot() . '' . dirname(__DIR__));
+            }
+            else {
+                return dirname(__DIR__);
+            }
+
+        }
+
     }
-
-    function InstallEvents() {
-
-
-      $obEventType = new CEventType;
-      $obEventType->Add([
-        "EVENT_NAME"  => "GENERATE_PWD",
-        "NAME"        => "Сгенерирован новый пароль",
-        "LID"         => "ru",
-        "DESCRIPTION" => "#NEWPWD# - Сгенерированный пароль",
-      ]);
-      $arSites = [];
-      $sites   = CSite::GetList($b = "", $o = "", [LANGUAGE_ID => "ru"]);
-      while ($site = $sites->Fetch()) {
-        $arSites[] = $site[ "LID" ];
-      }
-      if (count($arSites) > 0) {
-        $ar[ "ACTIVE" ]     = "Y";
-        $ar[ "EVENT_NAME" ] = "GENERATE_PWD";
-        $ar[ "EMAIL_FROM" ] = "#DEFAULT_EMAIL_FROM#";
-        $ar[ "LID" ]        = $arSites;
-        $ar[ "EMAIL_TO" ]   = "#EMAIL_TO#";
-        $ar[ "SUBJECT" ]    = "Сгенерирован новый пароль";
-        $ar[ "BODY_TYPE" ]  = "text";
-        $ar[ "MESSAGE" ]    = "Вам сгенерирован новый пароль для входа. #NEWPWD#";
-        $emess              = new CEventMessage;
-        $emess->Add($ar);
-      }
-      return TRUE;
-    }
-
-    function UnInstallEvents() {
-      $obEventType = new CEventType;
-      $obEventType->Delete("GENERATE_PWD");
-      $arFilter = [
-        "TYPE_ID" => "GENERATE_PWD",
-      ];
-      $rsMess   = CEventMessage::GetList($by = "site_id", $order = "desc", $arFilter);
-
-      while ($arMess = $rsMess->GetNext()) {
-        $emess = new CEventMessage;
-        $emess->Delete($arMess[ 'ID' ]);
-      }
-      return TRUE;
-    }
-
-    function InstallDB() {
-      RegisterModuleDependences('main', 'OnBeforeUserSendPassword',
-        'my.module', 'GeneratePassword', 'OnBeforeUserSendPasswordHandler');
-
-      return TRUE;
-    }
-
-    function UnInstallDB() {
-
-      UnRegisterModuleDependences('main', 'OnBeforeUserSendPassword',
-        'my.module', 'GeneratePassword', 'OnBeforeUserSendPasswordHandler');
-
-      return TRUE;
-
-    }
-
-    function DoInstall() {
-
-      $this->InstallDB();
-      $this->InstallEvents();
-      ModuleManager::registerModule($this->MODULE_ID);
-    }
-
-    function DoUninstall() {
-      ModuleManager::unregisterModule($this->MODULE_ID);
-      $this->UnInstallEvents();
-      $this->UninstallDB();
-    }
-
-
-    public function getpath($notdocumentroot = FALSE) {
-      if ($notdocumentroot) {
-        return str_ireplace(\Bitrix\Main\Application::getDocumentRoot() . '' . dirname(__DIR__));
-      }
-      else {
-        return dirname(__DIR__);
-      }
-
-    }
-
-  }
 
 
 ?>
